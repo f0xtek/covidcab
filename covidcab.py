2d90b6949a025feaddab84a247e0ff7b08424846
Luke Anderson <landerson61@gmail.com>
2020-08-18 14:55:18 +0100

    Initial commit

---
+++ covidcab.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+
+def main():
+    # Use a breakpoint in the code line below to debug your script.
+    print('Hi')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+if __name__ == '__main__':
+    main()
